6e5cf13c06a1bdefdb4650274a415ceb1ff608f6
renjianyong <renjianyong0923@gmail.com>
2024-06-07 16:09:00 +0800

    更新.

--- media/mediainfo.py
+++ media/mediainfo.py
@@ -27,35 +27,102 @@ def get_files_item(file_dir: str, file_ext=DEFAULT_FILE_EXT):
     return files_list
 
 
-def get_mediainfo(file_list: list):
+def get_mediainfo(data_list: list):
     print('---------------------------------------------------------------------')
+    for data in data_list:
+        #
+        print(f"{data['file_name']}\n")
+        print('编码格式:', data['v_code_name'])
+        print('分辨率:', data['v_resolution'])
+        print('帧率模式:', data['v_frame_rate_mode'])
+        print(f"帧率:{data['v_frame_rate']}\n")
+        print('数据速率(Kbps):', data['v_k_bit_rate'])
+        print(f"数据速率(Mbps):{data['v_m_bit_rate']}\n")
+        print('混音模式:', data['a_channel_type'])
+        print('音频格式:', data['a_commercial_name'])
+        print('音频采样率:', data['a_sampling_rate'])
+        print('音频比特率:', data['a_bit_rate'])
+        print('---------------------------------------------------------------------')
+
+
+def mediainfo_handler(file_list: list):
+    '''
+    处理通过 MediaInfo 工具直接返回的数据，然后封装为对象.
+    Args:
+        file_list:视频文件列表
+
+    Returns:包含封装好的视频信息对象的列表
+
+    '''
+    media_data_list = []
     for file in file_list:
-        file_path_item = os.path.split(file)
-        print(file_path_item[-1])
+        # 文件名
+        media_data = {'file_name': os.path.split(file)[-1]}
+
         json_data = json.loads(MediaInfo.parse(file).to_json())
         for file_obj in json_data['tracks']:
             track_type = file_obj['track_type']
+            # 视频信息
             if 'Video' == track_type:
-                print('编码格式:',
-                      file_obj['internet_media_type'] + r' ' + file_obj['commercial_name'] + r' (' + file_obj[
-                          'format_info'] + r')')
-                print('分辨率:', str(file_obj['width']) + r' x ' + str(file_obj['height']))
+                # 视频编码格式
+                internet_media_type = file_obj['internet_media_type']
+                if 'H265' in internet_media_type or 'h265' in internet_media_type:
+                    internet_media_type = 'H.265'
+                elif 'H264' in internet_media_type or 'H264' in internet_media_type:
+                    internet_media_type = 'H.264'
+
+                media_data['v_code_name'] = internet_media_type + r' ' + file_obj['commercial_name'] + r' (' + file_obj[
+                    'format_info'] + r')'
+
+                # 分辨率
+                media_data['v_resolution'] = str(file_obj['width']) + r' x ' + str(file_obj['height'])
+
+                # 帧率模式
+                media_data['v_frame_rate_mode'] = str(file_obj['width']) + r' x ' + str(file_obj['height'])
+
                 frame_rate_mode = file_obj['frame_rate_mode']
                 if 'CFR' == frame_rate_mode:
-                    print('帧率模式:','CFR (恒定帧率)')
+                    media_data['v_frame_rate_mode'] = 'CFR (恒定帧率)'
                 elif 'VFR' == frame_rate_mode:
-                    print('帧率模式:', 'VFR (可变帧率)')
-                print('帧率:', file_obj['other_frame_rate'][0])
+                    media_data['v_frame_rate_mode'] = 'VFR (可变帧率)'
+
+                # 帧率
+                media_data['v_frame_rate'] = file_obj['other_frame_rate'][0]
+
+                # 码率
                 bit_rate = file_obj['bit_rate']
-                print('\n数据速率(Kbps):', str(Decimal(int(bit_rate) / 1000).quantize(Decimal("0"))))
-                print('数据速率(Mbps):', str(Decimal(int(bit_rate) / 1000 / 1000).quantize(Decimal("0.00"))),'\n')
+                media_data['v_k_bit_rate'] = str(Decimal(int(bit_rate) / 1000).quantize(Decimal("0")))
+                media_data['v_m_bit_rate'] = str(Decimal(int(bit_rate) / 1000 / 1000).quantize(Decimal("0.00")))
+            # 音频信息
             elif 'Audio' == track_type:
+                # 混响模式
                 channel_s = file_obj['channel_s']
                 if 2 == channel_s:
-                    print('混音模式:', 'Stereo (立体声)')
+                    media_data['a_channel_type'] = 'Stereo (立体声)'
                 else:
-                    print('混音模式:', '其它')
-                print('音频格式:', file_obj['commercial_name'] + r' (' + file_obj['format_info'] + r')')
-                print('音频采样率:', file_obj['other_sampling_rate'][0])
-                print('音频比特率:', file_obj['other_bit_rate'][0])
-        print('---------------------------------------------------------------------')
+                    media_data['a_channel_type'] = '其它'
+
+                # 音频格式
+                media_data['a_commercial_name'] = file_obj['commercial_name'] + r' (' + file_obj['format_info'] + r')'
+
+                # 音频采样率
+                media_data['a_sampling_rate'] = file_obj['other_sampling_rate'][0]
+
+                # 音频比特率
+                media_data['a_bit_rate'] = file_obj['other_bit_rate'][0]
+        media_data_list.append(media_data)
+    return media_data_list
+
+
+def mediainfo_(file_path: str):
+    '''
+    调用触发函数
+    Args:
+        file_path:给定的文件路径或文件夹路径.
+
+    Returns:
+
+    '''
+    file_path_items = get_files_item(file_path)
+    file_datas = mediainfo_handler(file_path_items)
+    get_mediainfo(file_datas)
